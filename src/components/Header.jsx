58c7a4f6932acad2b00e1f8742b0f23a73a1ea76
Lucas G Silveira <lucgs88@gmail.com>
2024-10-25 20:34:40 -0300

    clever way to create buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 function Header() {
+  const navItems = ["Como ajudar", "Sobre Nós", "Animais para Adoção", "Loja", "Eventos", "Voluntariado","Contato"];
+
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-100 p-4 flex justify-between items-center shadow-lg">
       {/* Left side for logo */}
@@ -11,13 +13,11 @@ function Header() {
       {/* Right side for navigation */}
       <nav className="w-3/4 flex justify-end pr-8">
         <div className="space-x-4">
-          <button className="bg-blue-400 text-black font-cabin">Como ajudar</button>
-          <button className="bg-gray-100 text-black">Sobre Nós</button>
-          <button className="bg-gray-100 text-black">Animais para Adoção</button>
-          <button className="bg-gray-100 text-black">Loja</button>
-					<button className="bg-gray-100 text-black">Eventos</button>
-          <button className="bg-gray-100 text-black">Voluntariado</button>
-          <button className="bg-gray-100 text-black">Contato</button>
+          { navItems.map((item) => (
+            <button key={ item } className="bg-gray-100 text-black">
+              {item}
+            </button>
+          ))}
         </div>
       </nav>
     </header>
